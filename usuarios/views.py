584277007d4c8a0ba386a38d0b3f365d41807800
eeyMatheusDev <151544607+eeyMatheusDev@users.noreply.github.com>
2024-04-16 20:50:58 -0300

    feat: add initial cadastro_medico

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -37,3 +38,23 @@ def cadastro(request):
         except:
             print('Erro 4')
             return redirect('/usuarios/cadastro')
+            
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+        return redirect('/usuarios/login')
+    
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')    
