185f491d8f614d45020f6e332ed1916f8a8ea587
eeyMatheusDev <151544607+eeyMatheusDev@users.noreply.github.com>
2024-04-29 18:02:47 -0300

    feat: remove debbug

--- healing/settings.py
+++ healing/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#014zupiqt!rz%be(dvnf$h5+f(u83)t9x)yks)vov_p0@zcu4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
